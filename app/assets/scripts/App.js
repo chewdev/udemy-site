b2e9a44ebb1abf803246953e939a0dbb5c60c1dc
chewdev <itsbewald@gmail.com>
2019-03-23 19:04:21 -0700

    Add completed modal overlay.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,9 +1,11 @@
 import MobileMenu from "./modules/MobileMenu";
 import RevealOnScroll from "./modules/RevealOnScroll";
 import StickyHeader from "./modules/StickyHeader";
+import Modal from "./modules/Modal";
 
 var mobileMenu = new MobileMenu();
 new RevealOnScroll(".feature-item", "80%");
 new RevealOnScroll(".testimonial", "70%");
 new RevealOnScroll(".testimonial--last", "90%");
 new StickyHeader();
+new Modal();
